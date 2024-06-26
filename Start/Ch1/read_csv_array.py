6bd7d5e125bcf30739e66c3ad96259c92a2272d1
Murat <4118748+mshagirov@users.noreply.github.com>
2024-06-26 05:46:00 +0000

    ch1

--- Start/Ch1/read_csv_array.py
+++ Start/Ch1/read_csv_array.py
@@ -5,12 +5,21 @@
 # import the csv module from the standard library
 import csv
 
+
 def read_csv_to_array(filename):
   # define the array that will hold the data
   data = []
+  with open(filename, 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+      data.append(row)
+  return data
 
 # Read the data into an array of arrays
 inventory_data = read_csv_to_array("Inventory.csv")
 
+print(f"Items: {len(inventory_data)}")
+print(inventory_data[0])
+print(inventory_data[1])
 # Each row in the array is itself an array of values
 
--- Start/Ch1/read_csv_dict.py
+++ Start/Ch1/read_csv_dict.py
@@ -7,10 +7,19 @@ import pprint
 
 def read_csv_to_dict(filename):
   data = {}
-
+  with open(filename, 'r') as csvfile:
+    reader = csv.DictReader(csvfile)    
+    #row = next(reader) # read and skip header
+    key = 0
+    for row in reader:
+      data[key] = row
+      key += 1
   return data
 
 # Example usage
 inventory_data = read_csv_to_dict("Inventory.csv")
 
 # Accessing data
+pprint.pprint(inventory_data)
+pprint.pprint(inventory_data[0])
+pprint.pprint(inventory_data[0]["Consumer Price"])
